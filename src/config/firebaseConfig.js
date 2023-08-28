e7da1b427fba4b7bde171dbfc12875671ca61058
marpolll <minstarsoso@gmail.com>
2023-08-28 13:28:56 +0900

    문제해결시나리오

---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAvFVKvC_YAIQhLZcRMtuGLVDWMY32CTdo",
+  authDomain: "react-marpolll.firebaseapp.com",
+  projectId: "react-marpolll",
+  storageBucket: "react-marpolll.appspot.com",
+  messagingSenderId: "968494509314",
+  appId: "1:968494509314:web:1b443d1168edbb7aca261b",
+};
+
+// Initialize Firebase
+export const firebaseApp = initializeApp(firebaseConfig);
